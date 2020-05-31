b2bafe69e6ff07220cf69a41a9fe8ca1937ee53e
Connor Rayburn <connorrayburn.work@gmail.com>
2020-05-31 12:25:18 -0700

    file layout and paths created

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+
+const burger = {
+  all: async () => {
+    const result = await orm.all("burger");
+
+    return result;
+  },
+
+  // The variables cols and vals are arrays.
+  create: async (cols, vals) => {
+    const result = await orm.create("burger", cols, vals);
+
+    return result;
+  },
+
+  update: async (objColVals, condition) => {
+    const result = await orm.update("burger", objColVals, condition);
+
+    return result;
+  }
+};
+
+// Export the database functions for the controller (burgersController.js).
+module.exports = burger;
