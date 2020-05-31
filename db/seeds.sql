e906b1f3f5fe7a035578a5c57402dbd8c5b56edb
Connor Rayburn <connorrayburn.work@gmail.com>
2020-05-30 17:03:00 -0700

    ran schema and seed to create database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE burgers_db:
 
-INSERT INTO burgers (burger_name) VALUES (`Baconator`);
-INSERT INTO burgers (burger_name) VALUES (`Whopper`);
-INSERT INTO burgers (burger_name) VALUES (`Big Mac`);
+INSERT INTO burgers (burger_name) VALUES ("Baconator");
+INSERT INTO burgers (burger_name) VALUES ("Whopper");
+INSERT INTO burgers (burger_name) VALUES ("Big Mac");
